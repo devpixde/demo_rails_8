d6303a1bec41b96c03c927db908c52abeef31cf5
Ingo Klemm <info@devpix.de>
2024-12-06 11:17:40 +0100

    Add some pages - add root path to router

---
+++ app/controllers/cycle_controller.rb
@@ -0,0 +1,6 @@
+class CycleController < ApplicationController
+  def index
+    @data = %w(Germany United\ Kingdom France Italy Russia Spain)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/" => "start#index"
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -13,4 +13,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root "start#index"
+  get "/cycle" => "cycle#index"
+  get "/truncate" => "truncate#index"
+  get "/striptags" => "striptags#index"
+
 end
