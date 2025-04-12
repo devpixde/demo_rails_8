6d6405e70334a35e0e548b318989209e049d0036
Ingo Klemm <info@devpix.de>
2025-04-12 12:23:10 +0200

    Implements enum for Article status
    Implements an enum for the Article status, allowing for draft,
    published, archived, and deleted states.
    
    Updates the article form to use a select dropdown for the status.
    
    Adds status information and helper methods to the article show view.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  # enum :status, [ :draft, :published, :archived, :deleted ]
+  # enum :status, %i(draft published archived deleted)
+  # oben zwei Alternativen. Hash hat den Vorteil, dass die Reihenfolge keine Rolle spielt
+  enum :status, { draft: 0, published: 1, archived: 2, deleted: 3 }
+
 end
